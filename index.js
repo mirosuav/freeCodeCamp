deb6f1749c078a31d66ed7eb1b7b6e4aa9b0be88
Mirek <40789254+mirosuav@users.noreply.github.com>
2023-12-19 01:00:12 +0100

    Request Header Parser Microservice

--- index.js
+++ index.js
@@ -1,34 +1,104 @@
-// index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const { TableServiceClient, TableClient, AzureNamedKeyCredential, odata } = require("@azure/data-tables");
+const { DefaultAzureCredential } = require('@azure/identity');
+const express = require('express');
+const cors = require('cors');
+const { URL } = require('url');
+const dns = require('node:dns');
+const app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+// Basic Configuration
+const port = process.env.PORT || 3000;
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+const dbEndpoint = process.env.DB_ENDPOINT;
+const dbCredential = new DefaultAzureCredential()
 
-// http://expressjs.com/en/starter/basic-routing.html
+// new AzureNamedKeyCredential(
+//   "<account-name>",
+//   "<account-key>"
+// );
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/shorturl', async function (req, res) {
+  const orgUrl = req.body.url;
 
-app.get('/api/whoami', function (req, res) {  
-  res.json( {
-       "ipaddress": req.ip,
-       "language": req.get('accept-language'),
-       "software": req.get('user-agent')
-     });
+  if (validateUrl(orgUrl)) {
+
+    const shortUlr = await shortenUrl(orgUrl);
+
+    res.json({
+      original_url: orgUrl,
+      short_url: shortUlr
+    });
+
+  }
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.get('/api/shorturl/:shortUrl', async function (req, res) {
+  const orgUrl = await getFullUrl(shortUlr);
+  if (orgUrl) {
+    res.redirect(orgUrl);
+  }
+  else {
+    res.status(400);
+  }
 });
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
+
+function validateUrl(orgUrl) {
+  return new URL(orgUrl);
+}
+
+async function shortenUrl(orgUrl) {
+
+  const tableClient = new TableClient(
+    dbEndpoint,
+    'shortedurls',
+    dbCredential
+  );
+
+ const shortedUrl = hashCode(orgUrl);
+
+  const entry = {
+    partitionKey: "",
+    rowKey: shortedUrl.toString(),
+    url: orgUrl
+  };
+
+  await tableClient.createEntity(entry);
+
+  return shortedUrl;
+};
+
+async function getFullUrl(shortUrl) {
+  const tableClient = new TableClient(
+    dbEndpoint,
+    'shortedurls',
+    dbCredential
+  );
+
+  let result = await tableClient.getEntity("", shortUrl)
+  .catch((error) => {
+    return undefined;
+  });
+
+  return result.url;
+};
+
+function hashCode(s) {
+  for(var i = 0, h = 0; i < s.length; i++)
+      h = Math.imul(31, h) + s.charCodeAt(i) | 0;
+  return h;
+}
