bbc68654f49890f125704ffef4c11a534280d3e8
Mirek <40789254+mirosuav@users.noreply.github.com>
2023-12-28 02:38:51 +0100

    filemetadata initial solution

--- index.js
+++ index.js
@@ -1,104 +1,20 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+var express = require('express');
+var cors = require('cors');
 require('dotenv').config()
-const bodyParser = require("body-parser");
-const { User, Exercise } = require("./domain.js");
 
-app.use(bodyParser.json());
-app.use(cors())
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
+var app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
 
-
-//List all users 
-app.get('/api/users', async (req, res, next) => {
-  try {
-    const users = await User.fetchAll();
-    res.json(users.map(u => u.asResponse()));
-  }
-  catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
-
-
-//Create user
-app.post('/api/users', async (req, res, next) => {
-  try {
-    const user = await User.storeNew(req.body.username);
-    res.json(user.asResponse());
-  }
-  catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
-
-//Add exercises
-app.post('/api/users/:_id/exercises', async (req, res, next) => {
-  try {
-
-    console.log(`exercises: ${req.body.description}, ${req.body.duration}, ${req.body.date}`);
-
-    let user = await User.fetchById(req.params._id);
-    const exercise = await Exercise.storeNew(user, req.body.description, req.body.duration, req.body.date);
-    res.json(exercise.asResponse());
-  }
-  catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
-
-
-//Query user exercises
-//?[from][&to][&limit]
-app.get('/api/users/:_id/logs', async (req, res, next) => {
-  try {
-    const user = await User.fetchById(req.params._id);
-    if (!user) {
-      res.sendStatus(404);
-    }
-    else {
-      const logs = await Exercise.fetchLogArray(user, req.query.from, req.query.to, req.query.limit);
-
-      res.json({
-        username: user.name,
-        count: logs.length,
-        _id: user.id,
-        log: logs
-      })
-    }
-  }
-  catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
-
-app.get('/api/logs', async (req, res, next) => {
-  try {
-    const logs = await Exercise.fetchAll();
-
-    res.json(logs);
-
-  }
-  catch (err) {
-    console.error(err);
-    next(err);
-  }
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
 
-
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
